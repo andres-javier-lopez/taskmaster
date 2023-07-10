fb169b3f297c51c1caf7814084fcdf76fab1ace2
Andres Javier Lopez <code@andresjavierlopez.com>
2023-07-10 16:16:50 -0600

    Set up json encoder for entities

--- api/taskmaster/core/entities.py
+++ api/taskmaster/core/entities.py
@@ -1,8 +1,21 @@
-from dataclasses import dataclass, field
+import dataclasses
+import json
 from uuid import UUID, uuid4
 
+from taskmaster.core.encoder import CustomEncoder
 
-@dataclass
+
+@dataclasses.dataclass
 class Entity:
     # To avoid conflicts with inherited dataclasses use kw_only=True
-    uuid: UUID = field(default_factory=uuid4, kw_only=True)
+    uuid: UUID = dataclasses.field(default_factory=uuid4, kw_only=True)
+
+    def __post_init__(self):
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                new_value = field.type(value)
+                setattr(self, field.name, new_value)
+
+    def to_json(self):
+        return json.dumps(dataclasses.asdict(self), cls=CustomEncoder)
