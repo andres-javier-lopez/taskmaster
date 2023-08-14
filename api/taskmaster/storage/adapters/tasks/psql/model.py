f27cb021c053d9adc820f738ed73ec89b7ed7eb6
Andres Javier Lopez <code@andresjavierlopez.com>
2023-08-14 12:10:58 -0600

    Add generic functions to convert from models to entities

--- api/taskmaster/storage/adapters/tasks/psql/model.py
+++ api/taskmaster/storage/adapters/tasks/psql/model.py
@@ -10,6 +10,7 @@ from taskmaster.domain.tasks.entities import Task
 
 
 class TaskModel(BaseModel):
+    ModelEntity = Task
     __tablename__ = "tasks"
 
     title: Mapped[str]
@@ -23,32 +24,4 @@ class TaskModel(BaseModel):
     task_mood: Mapped[Task.Mood]
 
     def to_entity(self) -> Task:
-        return Task(
-            uuid=self.uuid,
-            index=self.index,
-            title=self.title,
-            description=self.description,
-            status=self.status,
-            due_date=self.due_date,
-            scheduled_date=self.scheduled_date,
-            scheduled_time=self.scheduled_time,
-            estimated_effort_hours=self.estimated_effort_hours,
-            must_be_done=self.must_be_done,
-            task_mood=self.task_mood,
-        )
-
-    @classmethod
-    def from_entity(cls, entity: Task) -> TaskModel:
-        return cls(
-            uuid=entity.uuid,
-            index=entity.index,
-            title=entity.title,
-            description=entity.description,
-            status=entity.status,
-            due_date=entity.due_date,
-            scheduled_date=entity.scheduled_date,
-            scheduled_time=entity.scheduled_time,
-            estimated_effort_hours=entity.estimated_effort_hours,
-            must_be_done=entity.must_be_done,
-            task_mood=entity.task_mood,
-        )
+        return super().to_entity()
