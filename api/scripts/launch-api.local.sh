ddb78894c1989603d8c54f29be8d616ec19dc85d
Andres Javier Lopez <code@andresjavierlopez.com>
2024-05-13 13:10:38 -0400

    Configure alembic on project

--- api/scripts/launch-api.local.sh
+++ api/scripts/launch-api.local.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+export ALEMBIC_CONFIG=taskmaster/core/sqlalchemy/alembic.ini
+poetry run alembic upgrade head
 poetry run python -m uvicorn taskmaster.app:app --host 0.0.0.0 --port 8001 --reload --log-level info
--- api/taskmaster/settings.py
+++ api/taskmaster/settings.py
@@ -1,4 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 REDIS_URL = "redis"
 PSQL_CONNECTION_STRING = os.getenv("PSQL_CONNECTION_STRING")
